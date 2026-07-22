a20ece0d8b85af69cb1f444cb3654846d8dd0b91
ustiaserg <ustiaserg@gmail.com>
2026-07-22 11:28:58 +0200

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import CashFlowPlanner from './App.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <CashFlowPlanner />
-);
